5f485245a183bcf9b7073d43bc5cd6b55cb17f03
Apoorv Taneja <apoorvtaneja@Apoorvs-Air.domain.name>
2020-11-15 22:15:12 +0530

    changes in CSS of Addtier Modal

--- components/AddTierModal/index.js
+++ components/AddTierModal/index.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 const ModalContainer = styled.div`
-.absolute;
+.fixed;
 .top-50%;
 .left-50%;
-transform: translate(-50%,0%);
+transform: translate(-50%,-50%);
 background: white;
 .rounded;
 width:40vw;
 height:400px;
+overflow-y: scroll;
 border: 0.5px solid #c8c8c8;
 .shadow-2xl;
 @media(max-width:500px){
