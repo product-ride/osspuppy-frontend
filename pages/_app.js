4314653326bb5ac39138cc84eb75d70a1d291c31
Ameer Jhan <ameerjhanprof@gmail.com>
2021-01-27 17:06:05 +0530

    fix: global style injection

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout';
-import { ServerStyleSheet } from 'styled-components';
 import AuthProvider from '../contexts/auth/auth';
 
 function MyApp({ Component, pageProps }) {
@@ -9,32 +8,6 @@ function MyApp({ Component, pageProps }) {
       <Layout {...pageProps}><Component {...pageProps} /></Layout>
     </AuthProvider>
   )
-}
-
-export async function getInitialProps(ctx) {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
-
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          sheet.collectStyles(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    sheet.seal();
-  }
 }
 
 export default MyApp
